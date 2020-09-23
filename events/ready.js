ba64dca1a6f26b32f844d13906d8d183729d7b22
Glitch (ripple-flax-sphynx) <none>
2020-09-23 15:28:29 +0000

    🌵🏃 Checkpoint ./events/ready1.js:21862579/2175

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,41 @@
-module.exports = async (client) => {
-  console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("/help", {
-    type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
+module.exports = class extends Event {
+  constructor(...args) {
+    super(...args, {
+      once: true
+    });
+  }
+
+  run() {
+    console.log(
+      [
+        `Logged in as ${this.client.user.tag}`,
+
+        `Loaded ${this.client.commands.size} commands!`,
+
+        `Loaded ${this.client.events.size} events!`
+      ].join("\n")
+    );
+
+    const activities = [
+      `${this.client.guilds.cache.size} servers!`,
+
+      `${this.client.channels.cache.size} channels!`,
+
+      `${this.client.guilds.cache.reduce(
+        (a, b) => a + b.memberCount,
+        0
+      )} users!`
+    ];
+
+    let i = 0;
+
+    setInterval(
+      () =>
+        this.client.user.setActivity(
+          `${this.client.prefix}help | ${activities[i++ % activities.length]}`,
+          { type: "WATCHING" }
+        ),
+      15000
+    );
+  }
 };
