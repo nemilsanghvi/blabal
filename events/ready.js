21efedf59d1683202c80fac21752445edb34fa48
Glitch (ripple-flax-sphynx) <none>
2020-09-23 15:52:45 +0000

    🐝🍞 Checkpoint ./events/ready.js:21862579/33

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,7 @@
 module.exports = async client => {
   console.log(`[API] Logged in as ${client.user.username}`);
 
-  await client.user.setActivity("Music" {
+  await client.user.setActivity("/help", {
     type: "LISTENING" //can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
