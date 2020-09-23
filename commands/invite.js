0cef78e99aa2f2a67051ca06ec3e0407afacba44
Glitch (ripple-flax-sphynx) <none>
2020-09-23 12:11:46 +0000

    👠🌗 Checkpoint ./commands/Invite.js:21862579/2012 ./commands/invite.js:21862579/2221

---
+++ commands/invite.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+
+const sendError = require("../util/error");
+
+module.exports = {
+  info: {
+    name: "Invite",
+
+    description: "To Show the invite link of bot",
+
+    usage: "",
+
+    aliases: []
+  },
+
+  run: async function(client, message, args) {
+    const embed = new Discord.MessageEmbed()
+
+      .setAuthor("Invite Link of Bot")
+
+      .setColor("BLUE")
+
+      .setDescription(
+        " [Invite Here](https://discord.com/oauth2/authorize?client_id=758235022120583168&permissions=268790848&scope=bot) "
+      )
+
+      .setFooter(
+        "Currently the bot is in Beta if any problems arise do contact in the support server [Support](https://discord.gg/3K4HeYz)"
+      );
+    message.channel.send(embed);
+  }
+};
